75bf0ac93b5a52d3f45cd470b6a194e117386b97
Black Box <developer.luke.barker3@outlook.com>
2022-06-20 21:47:12 +0100

    Implement ExtractSubstring extension method

---
+++ src/StreamInstruments.Extensions/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace StreamInstruments.Extensions;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Extracts a substring between the first occurrence of <see cref="start"/>
+    /// and the last occurrence of <see cref="end"/>.
+    /// </summary>
+    /// <param name="toExtractFrom">The string to extract from.</param>
+    /// <param name="start">Where to start extracting from in the string <see cref="toExtractFrom"/></param>
+    /// <param name="end">Where to stop extracting from in the string <see cref="toExtractFrom"/></param>
+    /// <returns>
+    /// The substring between the first occurrence of <see cref="start"/> and the last occurrence of <see cref="end"/>.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    /// When either the <see cref="start"/> or <see cref="end"/> string does not appear in the
+    /// string <see cref="toExtractFrom"/>.
+    /// </exception>
+    public static string ExtractSubstring(this string toExtractFrom, string start, string end)
+    {
+        var startIndex = toExtractFrom.IndexOf(start, StringComparison.Ordinal);
+        var endIndex = toExtractFrom.LastIndexOf(end, StringComparison.Ordinal);
+
+        if (startIndex < 0)
+        {
+            throw new ArgumentException($"'{start}' does not appear in string '{toExtractFrom}'.");
+        }
+
+        if (endIndex < 0)
+        {
+            throw new ArgumentException($"'{end}' does not appear in string '{toExtractFrom}'.");
+        }
+
+        if (startIndex > endIndex)
+        {
+            throw new ArgumentException($"{nameof(startIndex)} cannot be ahead of {nameof(endIndex)}.");
+        }
+
+        var startExtractingFromIndex = startIndex + start.Length;
+
+        return toExtractFrom[startExtractingFromIndex..endIndex];
+    }
+}
---
+++ tests/StreamInstruments.Extensions.Tests/StringExtensionTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace StreamInstruments.Extensions.Tests;
+
+[TestFixture]
+public class ExtractString_Extension_Method
+{
+    [Test]
+    [TestCase("", "", "", "")]
+    [TestCase("(123)", "(", ")", "123")]
+    [TestCase("(4, 5, 6, 7, 8, 9)", "(", ")", "4, 5, 6, 7, 8, 9")]
+    [TestCase("(abcdefghijklmn())", "(", ")", "abcdefghijklmn()")]
+    [TestCase("(abcdefghijklmn())", "(", "))", "abcdefghijklmn(")]
+    [TestCase("(abcdefghijklmn())", "abc", "ghi", "def")]
+    [TestCase("(abcdefghijklmn())", "abcdefghijklmn", "()", "")]
+    [TestCase("(abcdefghijklmn())", "", "", "(abcdefghijklmn())")]
+    public void Returns_Correct_Substring(string toExtractFrom, string start, string end, string expectedSubstring)
+    {
+        toExtractFrom.ExtractSubstring(start, end).Should().Be(expectedSubstring);
+    }
+
+    [Test]
+    public void Throws_ArgumentException_On_Start_Not_Appearing_In_String_ToExtractFrom()
+    {
+        Action act = () => "def".ExtractSubstring("abc", "def");
+
+        act.Should().Throw<ArgumentException>().WithMessage("'abc' does not appear in string 'def'.");
+    }
+
+    [Test]
+    public void Throws_ArgumentException_On_End_Not_Appearing_In_String_ToExtractFrom()
+    {
+        Action act = () => "abc".ExtractSubstring("abc", "def");
+
+        act.Should().Throw<ArgumentException>().WithMessage("'def' does not appear in string 'abc'.");
+    }
+
+    [Test]
+    public void Throws_ArgumentException_On_Start_String_Located_Ahead_Of_End_String_In_String_ToExtractFrom()
+    {
+        Action act = () => "abcdefghijklmn".ExtractSubstring("ghi", "abc");
+
+        act.Should().Throw<ArgumentException>().WithMessage("startIndex cannot be ahead of endIndex.");
+    }
+}
