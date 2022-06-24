c0e213073ce2fd3bd3b2de6d6580e5fdc6d0fffd
Black Box <developer.luke.barker3@outlook.com>
2022-06-24 20:01:36 +0100

    CoinModule tests

---
+++ tests/StreamInstruments.Hubs.Commands.Modules.Tests/CoinsModuleTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StreamInstruments.Hubs.Commands.Modules.ModuleImplementations;
+
+namespace StreamInstruments.Hubs.Commands.Modules.Tests;
+
+[TestFixture]
+public class A_CoinModule
+{
+    [Test]
+    public void Returns_Heads_Or_Tails()
+    {
+        var module = new CoinModule();
+
+        module.Toss().ToLower().Should().BeOneOf("heads", "tails");
+    }
+}
